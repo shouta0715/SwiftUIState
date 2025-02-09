3c14ed77bb4c2c0e4e3707e90589493094490243
shouta0715 <kshouta0715@gmail.com>
2025-02-09 10:42:58 +0900

    Initial Commit

---
+++ SwiftUIState/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIState
+//
+//  Created by k21047kk on 2025/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIState/SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateApp.swift
+//  SwiftUIState
+//
+//  Created by k21047kk on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
